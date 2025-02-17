294990f3c43b263e0c3f5a8e862c02f930bbb576
Quan Huy <quan.tuan.huy@gmail.com>
2025-02-17 22:09:44 +0700

    init project

---
+++ authentication_service/src/test/java/huy/project/authentication_service/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package huy.project.authentication_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
