d364df5edca0f84572ec4ca32ed4611e8562869d
Quan Huy <quan.tuan.huy@gmail.com>
2025-02-27 13:46:17 +0700

    init accommodation_service

---
+++ accommodation_service/src/test/java/huy/project/accommodation_service/AccommodationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package huy.project.accommodation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommodationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
