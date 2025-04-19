5dabee9e01ea8b32d53f2294913f0d7cdc828149
Quan Huy <quan.tuan.huy@gmail.com>
2025-04-19 22:15:53 +0700

    hotel detail page

--- web_ui/src/components/Footer/Footer.tsx
+++ web_ui/src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
     <footer className="bg-gray-100 py-6 md:py-8">
       <div className="container mx-auto px-4">
         <div className="text-center text-gray-600 text-sm">
-          <p>© 2025 Booking.com™. All rights reserved.</p>
+        <p>© 2025 Hotel Booking System. Tất cả các quyền được bảo lưu.</p>
         </div>
       </div>
     </footer>
